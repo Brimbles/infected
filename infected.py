884edeacb192f174aa127cc41fa253c1dcd3edd6
Ben Rimington <benrimington@googlemail.com>
2020-05-04 14:14:47 +0100

    zombies now green

--- infected.py
+++ infected.py
@@ -4,21 +4,21 @@ import random
 
 
 
-def pixiequeen_animation():
-    global pixiequeen_speed_x, pixiequeen_speed_y
-    pixiequeen.x += pixiequeen_speed_x
-    pixiequeen.y += pixiequeen_speed_y
+def humanbirther_animation():
+    global humanbirther_speed_x, humanbirther_speed_y
+    humanbirther.x += humanbirther_speed_x
+    humanbirther.y += humanbirther_speed_y
 
-    if pixiequeen.top <= 0 or pixiequeen.bottom >= screen_height:
-        pixiequeen_speed_y *= -1
-    if pixiequeen.left <= 0 or pixiequeen.right >= screen_width:
-        pixiequeen_speed_x *= -1
+    if humanbirther.top <= 0 or humanbirther.bottom >= screen_height:
+        humanbirther_speed_y *= -1
+    if humanbirther.left <= 0 or humanbirther.right >= screen_width:
+        humanbirther_speed_x *= -1
 
-    if pixiequeen.collidelist(population) or pixiequeen.collidelist(infectedpopulation):
-        #pixiequeen_speed_x *= -1
+    if humanbirther.collidelist(population) or humanbirther.collidelist(infectedpopulation):
+        #humanbirther_speed_x *= -1
         if random.randint(1, 100) == 1: #1% chance of direction change
-            pixiequeen_speed_x = 1 * random.choice((1, -1))
-            pixiequeen_speed_y = 1 * random.choice((1, -1))
+            humanbirther_speed_x = 1 * random.choice((1, -1))
+            humanbirther_speed_y = 1 * random.choice((1, -1))
 
 def personanimation():
     person.y = person.y + random.randint(-2, 2)
@@ -31,12 +31,12 @@ def personanimation():
 
 
 
-# def pixiequeen_start():
-#     global pixiequeen_speed_x, pixiequeen_speed_y
+# def humanbirther_start():
+#     global humanbirther_speed_x, humanbirther_speed_y
 
-#     pixiequeen.center = (screen_width / 2, screen_height / 2)
-#     pixiequeen_speed_y *= random.choice((1, -1))
-#     pixiequeen_speed_x *= random.choice((1, -1))
+#     humanbirther.center = (screen_width / 2, screen_height / 2)
+#     humanbirther_speed_y *= random.choice((1, -1))
+#     humanbirther_speed_x *= random.choice((1, -1))
 
 # def create_red_square(x, y):
 #     red_width = 10
@@ -60,7 +60,7 @@ clock = pygame.time.Clock()
 myfont = pygame.font.SysFont("monospace", 16)
 
 # Main Window
-screen_width = 500
+screen_width = 1200
 screen_height =  600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('infected')
@@ -71,6 +71,7 @@ bg_color = pygame.Color('grey12')
 red_color = pygame.Color("red")
 white_color = pygame.Color("white")
 blue_color = pygame.Color("blue")
+green_color = pygame.Color("green")
 ####################################  my game variables   ###########################
 
 # List of people - they should be born at the birth rate
@@ -94,12 +95,12 @@ lessthan80 = 0.05
 olderthan80 = 0.9
 
 # Game Rectangles
-pixiequeen = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 10, 10)  # pixiequeen starting position and size
+humanbirther = pygame.Rect(screen_width / 2 - 15, screen_height / 2 - 15, 10, 10)  # humanbirther starting position and size
 
 # Game Variables
 counter = 1
-pixiequeen_speed_x = 1 * random.choice((1, -1))
-pixiequeen_speed_y = 1 * random.choice((1, -1))
+humanbirther_speed_x = 1 * random.choice((1, -1))
+humanbirther_speed_y = 1 * random.choice((1, -1))
 
 collisiontext = "no collision yet"
 
@@ -118,8 +119,8 @@ while True:
     for a in population:
         for b in infectedpopulation:
             if a.colliderect(b) and a != b:
-                if int(counter/60) >=60: #At least 60 days have passed
-                    if random.randint(1, 100) ==1: #1 percent chance of infection:
+                if int(counter/60) >=20: #At least 20 days have passed
+                    if random.randint(1, 2) == 1: #50 percent chance of infection:
                         infectedpopulation.append(a)  # add person to the infected population
                         if a in population:
                             population.remove(a) #remove person from the non-infected population
@@ -136,8 +137,8 @@ while True:
     # 			infectedpopulation.append(person)#add person to the infected population
     # 		population.remove(person)
 
-    # if pixiequeen.colliderect(player) or pixiequeen.colliderect(opponent):
-    # 	pixiequeen_speed_x *= -1
+    # if humanbirther.colliderect(player) or humanbirther.colliderect(opponent):
+    # 	humanbirther_speed_x *= -1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -158,19 +159,19 @@ while True:
     # birth a new pixie
     if int(counter/60) % 10 == 0: #every 10 seconds
         if random.randint(1, 10) ==1: #10 percent chance of birth
-            pos_left, pos_top = pixiequeen.left, pixiequeen.top
+            pos_left, pos_top = humanbirther.left, humanbirther.top
             population.append(birth_new_person(pos_left + 2, pos_top + 2)) 
 
     # move pixie to infected population
 
     # Game Logic
-    pixiequeen_animation()
+    humanbirther_animation()
     counter += 1
 
 
     # Visuals
     screen.fill(bg_color)
-    pygame.draw.ellipse(screen, bg_color, pixiequeen)
+    pygame.draw.ellipse(screen, bg_color, humanbirther)
     #pygame.draw.aaline(screen, light_grey, (screen_width / 2, 0),(screen_width / 2, screen_height))
     for person in population:
         #screen.fill(red_color, person)#display all people
@@ -178,22 +179,28 @@ while True:
         #pygame.draw.rect(screen,white_color,person)
     for person in infectedpopulation:
         #screen.fill(red_color, person)#display all people
-        pygame.draw.ellipse(screen, red_color, person)
+        if int(counter/60) >=20: #At least 20 days have passed
+            pygame.draw.ellipse(screen, green_color, person)
         #pygame.draw.rect(screen,red_color,person)
 
     # display counter
 
-    elapsedtimetext = myfont.render("Elapsed time in days= " + str(int(counter/60)), 1, (200, 0, 0))
+    elapsedtimetext = myfont.render("Elapsed time in days= " + str(int(counter/60)), 1, light_grey)
     screen.blit(elapsedtimetext, (20, 20))
 	
-    populationtext = myfont.render("Uninfected population =" +str(len(population)), 1, (200, 0, 0))
+    populationtext = myfont.render("Human population =" +str(len(population)), 1, light_grey)
     screen.blit(populationtext, (20, 40))
 
-    infectedpopulationtext = myfont.render("Infected population =" +str(len(infectedpopulation)), 1, (200, 0, 0))
-    screen.blit(infectedpopulationtext, (40, 60))
+    if int(counter/60) >=20: #At least 20 days have passed
+        infectedpopulationtext = myfont.render("Zombie population =" +str(len(infectedpopulation)), 1, green_color)
+        screen.blit(infectedpopulationtext, (20, 60))
 
-    rulestext = myfont.render("Infection can only be be transmitted after day 60, each interaction has a 1 percent chance of transmission ", 1, (200, 0, 0))
-    screen.blit(rulestext, (40, 80))
+    rulestext = myfont.render("Infection can only be be transmitted after day 20", 1, light_grey)
+    screen.blit(rulestext, (20, 80))
+    rulestext1 = myfont.render("Each interaction has a 50 percent chance of transmission", 1, light_grey)
+    screen.blit(rulestext1, (20, 100))
+    rulestext2 = myfont.render("There's a chance the human population will boom every 10 days", 1, light_grey)
+    screen.blit(rulestext2, (20, 120))
 
     pygame.display.flip()
     clock.tick(60)
